b94493a2de8f3cc041c7116cd60e8d17eee90062
Jesper Hansen <hansen.jesper17@gmail.com>
2015-10-01 14:14:29 +0200

    Added the handleMsg()

---
+++ src/handleMessage.cpp
@@ -0,0 +1,173 @@
+/*
+ * handleMessage.cpp
+ *
+ * Author: Jesper Hansen
+ */
+
+#include "handleMessage.h"
+
+/**
+ * Function for handling the which Json message that Calvin-Base is sending.
+ * @param &msg JsonObject containing the message from Calvin-Base
+ */
+int MessageHandler::handleMsg(JsonObject &msg)
+{
+	/* This won't change, or we would lose the list in memory */
+	struct node *root;
+	/* This will point to each node as it traverse the list */
+	//struct node *conductor;
+
+	// Check if JsonObject msg contains the key: "cmd"
+	if (msg.containsKey("cmd"))
+	{
+		// Get the value from msg key "cmd"
+		const char* message = msg["cmd"];
+
+		/*switch (message) {
+			case "JOIN_REQUEST":
+				root = (node*) malloc(sizeof(nodes));
+
+				if (root == NULL)
+				{
+					return 9;
+				}
+
+				// These method will be activated in the final program
+				handleJoin(msg);
+				root->val = reply;
+
+				handleSetupTunnel(msg);
+				root->next = (node*) malloc(sizeof(struct node));
+				root->next->val = request;
+
+				return 1;
+			case "ACTOR_NEW":
+				root = (node*) malloc(sizeof(nodes));
+
+				if (root == NULL)
+				{
+					return 9;
+				}
+
+				// These method will be activated in the final program
+				root->val = handleActorNew(msg);
+				root->next = (node*) malloc(sizeof(struct node));
+				root->next->val = handleSetupPorts(msg);
+
+				return 2;
+			case "TUNNEL_DATA":
+				root = (node*) malloc(sizeof(nodes));
+				if (root == NULL)
+				{
+					return 9;
+				}
+
+				// These method will be activad in the final program
+				//root->val = handleTunnelData(msg);
+
+				return 3;
+			case "TOKEN":
+				root = (node*) malloc(sizeof(nodes));
+				if (root == NULL)
+				{
+					return 9;
+				}
+				// These method will be activated in the final program
+				//root->val = handleToken(msg);
+
+				return 4;
+			case "TOKEN_REPLY":
+			// These metod will be activated in the final program
+			// Return an empty list with an empty list within it
+
+			return 5;
+			case "REPLY":
+				// These metod will be activated in the final program
+				// Return an empty list
+
+				return 6;
+			default:
+				return 7;
+		}*/
+
+		if (!strcmp(message, "JOIN_REQUEST"))
+			{
+				root = (node*) malloc(sizeof(nodes));
+
+				if (root == NULL)
+				{
+					return 9;
+				}
+
+				// These method will be activated in the final program
+				/*handleJoin(msg);
+				root->val = reply;
+
+				handleSetupTunnel(msg);
+				root->next = (node*) malloc(sizeof(struct node));
+				root->next->val = request;*/
+
+				return 1;
+			}
+
+		if (!strcmp(message, "ACTOR_NEW"))
+		{
+			root = (node*) malloc(sizeof(nodes));
+			if (root == NULL)
+			{
+				return 9;
+			}
+
+			// These method will be activated in the final program
+			/*root->val = handleActorNew(msg);
+			root->next = (node*) malloc(sizeof(struct node));
+			root->next->val = handleSetupPorts(msg);*/
+
+			return 2;
+		}
+
+		if (!strcmp(message, "TUNNEL_DATA"))
+		{
+			root = (node*) malloc(sizeof(nodes));
+			if (root == NULL)
+			{
+				return 9;
+			}
+
+			// These method will be activad in the final program
+			//root->val = handleTunnelData(msg);
+
+			return 3;
+		}
+
+		if (!strcmp(message, "TOKEN"))
+		{
+			root = (node*) malloc(sizeof(nodes));
+			if (root == NULL)
+			{
+				return 9;
+			}
+			// These method will be activated in the final program
+			//root->val = handleToken(msg);
+
+			return 4;
+		}
+
+		if (!strcmp(message, "TOKEN_REPLY"))
+		{
+			// These metod will be activated in the final program
+			// Return an empty list with an empty list within it
+
+			return 5;
+		}
+
+		if (!strcmp(message, "REPLY")) {
+			// These metod will be activated in the final program
+			// Return an empty list
+
+			return 6;
+		}
+		return 7;
+	}
+	return 8;
+}
---
+++ tests/test_handleMessage.cpp
@@ -0,0 +1,51 @@
+/*
+ * test_handleMessage.cpp
+ *
+ *  Created on: 27 sep. 2015
+ *      Author: Jesper Hansen
+ */
+#ifdef _MOCK_
+
+#include "gtest/gtest.h"
+#include "../src/handleMessage.h"
+
+class test_handleMessage : public ::testing::Test {
+protected:
+	virtual void SetUp() {}
+	virtual void TearDown() {}
+};
+
+TEST(test_handleMessage, handleMsg) {
+	MessageHandler messageHandler;
+
+	// Create an JsonObject with an StaticJsonBuffer
+	StaticJsonBuffer<1000> jsonBuffer;
+	JsonObject &msg = jsonBuffer.createObject();
+
+	// Test JOIN_REQUEST in handleMsg
+	msg["cmd"] = "JOIN_REQUEST";
+	EXPECT_EQ(1, messageHandler.handleMsg(msg));
+
+	// Test ACTOR_NEW in handleMsg
+	msg["cmd"] = "ACTOR_NEW";
+	EXPECT_EQ(2, messageHandler.handleMsg(msg));
+
+	// Test TUNNEL_DATA in handleMsg
+	msg["cmd"] = "TUNNEL_DATA";
+	EXPECT_EQ(3, messageHandler.handleMsg(msg));
+
+	// Test TOKEN in handleMsg
+	msg["cmd"] = "TOKEN";
+	EXPECT_EQ(4, messageHandler.handleMsg(msg));
+
+	// Test TOKEN_REPLY in handleMsg
+	msg["cmd"] = "TOKEN_REPLY";
+	EXPECT_EQ(5, messageHandler.handleMsg(msg));
+
+	// Test REPLY in handleMsg
+	msg["cmd"] = "REPLY";
+	EXPECT_EQ(6, messageHandler.handleMsg(msg));
+
+}
+
+#endif
