8257e76c6d58e67b7a75d87674571c740dd75918
Jesper Hansen <hansen.jesper17@gmail.com>
2015-10-05 21:46:13 +0200

    Done for mergin to develop

--- tests/testHandleSetupTunnel.cpp
+++ tests/testHandleSetupTunnel.cpp
@@ -10,14 +10,40 @@
 #include "gtest/gtest.h"
 #include "../src/CalvinDone/CalvinMini.h"
 
-class test_setupTunnel : public ::testing::Test {
+class testHandleSetupTunnel : public ::testing::Test {
 protected:
 	virtual void SetUp() {}
 	virtual void TearDown() {}
 };
 
 // A test method for testing the method handleSetupTunnel()
-TEST(test_setupTunnel, handleSetupTunnel) {
+TEST(testHandleSetupTunnel, testSetStringInMethod) {
+	// Create an instance variable of the setupTunnel class
+	CalvinMini calvinMini;
+
+	// Create an empty JsonObject with an StaticJsonBuffer
+	StaticJsonBuffer<1000> jsonBuffer;
+	JsonObject &request = jsonBuffer.createObject();
+	JsonObject &msg = jsonBuffer.createObject();
+	JsonObject &policy = jsonBuffer.createObject();
+
+	// Add a string value to msg_uuid key
+	msg["id"] = "123456789";
+
+	// Test for initiate the method so that the empty JsonObject is being filled,
+	// if the object is valid it will return a 1. Otherwise 2.
+	calvinMini.handleSetupTunnel(msg ,request, policy);
+
+	// Test for checking if the string value in the JsonObject is equal to the set string
+	EXPECT_STREQ("MSG-12345678-9101-1123-1415-161718192021", request.get("msg_uuid"));
+	EXPECT_STREQ("calvin-miniscule", request.get("from_rt_uuid"));
+	EXPECT_STREQ(msg.get("id"), request.get("to_rt_uuid"));
+	EXPECT_STREQ("TUNNEL_NEW", request.get("cmd"));
+	EXPECT_STREQ("fake-tunnel", request.get("tunnel_id"));
+	EXPECT_STREQ("token", request.get("type"));
+}
+
+TEST(testHandleSetupTunnel, testSize) {
 	// Create an instance variable of the setupTunnel class
 	CalvinMini calvinMini;
 
@@ -36,50 +62,54 @@ TEST(test_setupTunnel, handleSetupTunnel) {
 
 	// Test for checking the size of the JsonObject
 	EXPECT_EQ(7, request.size());
+}
 
-	// Test for checking if the string value in the JsonObject is equal to the set string
-	EXPECT_STREQ("MSG-12345678-9101-1123-1415-161718192021", request.get("msg_uuid"));
-	EXPECT_STREQ("calvin-miniscule", request.get("from_rt_uuid"));
-	EXPECT_STREQ(msg.get("id"), request.get("to_rt_uuid"));
-	EXPECT_STREQ("TUNNEL_NEW", request.get("cmd"));
-	EXPECT_STREQ("fake-tunnel", request.get("tunnel_id"));
-	EXPECT_STREQ("token", request.get("type"));
+TEST(testHandleSetupTunnel, testContainsKey) {
+	// Create an instance variable of the setupTunnel class
+	CalvinMini calvinMini;
+
+	// Create an empty JsonObject with an StaticJsonBuffer
+	StaticJsonBuffer<1000> jsonBuffer;
+	JsonObject &request = jsonBuffer.createObject();
+	JsonObject &msg = jsonBuffer.createObject();
+	JsonObject &policy = jsonBuffer.createObject();
+
+	// Add a string value to msg_uuid key
+	msg["id"] = "123456789";
+
+	// Test for initiate the method so that the empty JsonObject is being filled,
+	// if the object is valid it will return a 1. Otherwise 2.
+	calvinMini.handleSetupTunnel(msg ,request, policy);
 
 	// Checks if JsonObject contains the following keys
+	EXPECT_TRUE(request.containsKey("msg_uuid"));
+	EXPECT_TRUE(request.containsKey("from_rt_uuid"));
+	EXPECT_TRUE(request.containsKey("to_rt_uuid"));
+	EXPECT_TRUE(request.containsKey("cmd"));
+	EXPECT_TRUE(request.containsKey("tunnel_id"));
+	EXPECT_TRUE(request.containsKey("type"));
 	EXPECT_TRUE(request.containsKey("policy"));
+}
+
+TEST(testHandleSetupTunnel, testPolicySize) {
+	// Create an instance variable of the setupTunnel class
+	CalvinMini calvinMini;
+
+	// Create an empty JsonObject with an StaticJsonBuffer
+	StaticJsonBuffer<1000> jsonBuffer;
+	JsonObject &request = jsonBuffer.createObject();
+	JsonObject &msg = jsonBuffer.createObject();
+	JsonObject &policy = jsonBuffer.createObject();
+
+	// Add a string value to msg_uuid key
+	msg["id"] = "123456789";
+
+	// Test for initiate the method so that the empty JsonObject is being filled,
+	// if the object is valid it will return a 1. Otherwise 2.
+	calvinMini.handleSetupTunnel(msg ,request, policy);
 
 	// Checks if the JsonObject policy is size 0
 	EXPECT_EQ(0, request.get("policy").size());
 }
 
-// A test for the handleTunnelData
-/*TEST(test_setupTunnel, handleTunnelData) {
-	// Create an instance variable of the setupTunnel class
-	SetupTunnel setupTunnel;
-
-	// Create an empty JsonObject with an StaticJsonBuffer
-	StaticJsonBuffer<300> jsonBuffer;
-	JsonObject &reply = jsonBuffer.createObject();
-
-	JsonObject &msg = jsonBuffer.createObject();
-	msg["to_rt_uuid"] = "1234";
-	msg["from_rt_uuid"] = "5678";
-
-	// Test for initiate the method so that the empty JsonObject is being filled,
-	// if the object is valid it will return a 1. Otherwise 2.
-	EXPECT_EQ(1, setupTunnel.handleTunnelData(msg ,reply));
-
-	// Test for checking the size of the JsonObject
-	EXPECT_EQ(5, reply.size());
-
-
-	// Test for checking if the string value in the JsonObject is equal to the set string
-	EXPECT_STREQ("5678", reply.get("to_rt_uuid"));
-	EXPECT_STREQ("1234", reply.get("from_rt_uuid"));
-
-	EXPECT_STREQ("TUNNEL_DATA", reply.get("cmd"));
-	EXPECT_STREQ("fake-tunnel", reply.get("tunnel_id"));
-	EXPECT_STREQ("foo", reply.get("value"));
-
-}*/
 #endif
--- tests/test_handleMessage.cpp
+++ tests/test_handleMessage.cpp
@@ -7,7 +7,7 @@
 #ifdef _MOCK_
 
 #include "gtest/gtest.h"
-#include "../src/CalvinInProgress/handleMessage.h"
+#include "../src/CalvinDone/CalvinMini.h"
 
 class test_handleMessage : public ::testing::Test {
 protected:
@@ -16,35 +16,41 @@ protected:
 };
 
 TEST(test_handleMessage, handleMsg) {
-	MessageHandler messageHandler;
+	CalvinMini messageHandler;
 
 	// Create an JsonObject with an StaticJsonBuffer
 	StaticJsonBuffer<1000> jsonBuffer;
 	JsonObject &msg = jsonBuffer.createObject();
+	JsonObject &reply = jsonBuffer.createObject();
+	JsonObject &request = jsonBuffer.createObject();
+	JsonObject &policy = jsonBuffer.createObject();
 
 	// Test JOIN_REQUEST in handleMsg
 	msg["cmd"] = "JOIN_REQUEST";
-	EXPECT_EQ(1, messageHandler.handleMsg(msg));
+	EXPECT_EQ(1, messageHandler.handleMsg(msg, reply, request, policy));
 
 	// Test ACTOR_NEW in handleMsg
 	msg["cmd"] = "ACTOR_NEW";
-	EXPECT_EQ(2, messageHandler.handleMsg(msg));
+	EXPECT_EQ(2, messageHandler.handleMsg(msg, reply, request, policy));
 
 	// Test TUNNEL_DATA in handleMsg
 	msg["cmd"] = "TUNNEL_DATA";
-	EXPECT_EQ(3, messageHandler.handleMsg(msg));
+	EXPECT_EQ(3, messageHandler.handleMsg(msg, reply, request, policy));
 
 	// Test TOKEN in handleMsg
 	msg["cmd"] = "TOKEN";
-	EXPECT_EQ(4, messageHandler.handleMsg(msg));
+	EXPECT_EQ(4, messageHandler.handleMsg(msg, reply, request, policy));
 
 	// Test TOKEN_REPLY in handleMsg
 	msg["cmd"] = "TOKEN_REPLY";
-	EXPECT_EQ(5, messageHandler.handleMsg(msg));
+	EXPECT_EQ(5, messageHandler.handleMsg(msg, reply, request, policy));
 
 	// Test REPLY in handleMsg
 	msg["cmd"] = "REPLY";
-	EXPECT_EQ(6, messageHandler.handleMsg(msg));
+	EXPECT_EQ(6, messageHandler.handleMsg(msg, reply, request, policy));
+
+	msg["cmd"] = "wrong";
+	EXPECT_EQ(7, messageHandler.handleMsg(msg, reply, request, policy));
 
 }
 
