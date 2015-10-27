3c0644134ecd4ca81e1ebd1165c006d6dc9e907f
aelvstam <andreas.elvstam@hotmail.com>
2015-10-27 13:02:55 +0100

    added cpp and h files fro rfid actor

---
+++ src/CalvinInProgress/rfidActor.cpp
@@ -0,0 +1,13 @@
+/*
+ * rfidActor.cpp
+ *
+ *  Created on: 27 okt. 2015
+ *      Author: Andreas
+ */
+
+#include "rfidActor.h"
+
+
+
+
+
---
+++ src/CalvinInProgress/rfidActor.h
@@ -0,0 +1,15 @@
+/*
+ * rfidActor.h
+ *
+ *  Created on: 27 okt. 2015
+ *      Author: Andreas
+ */
+
+#ifndef CALVININPROGRESS_RFIDACTOR_H_
+#define CALVININPROGRESS_RFIDACTOR_H_
+
+
+
+
+
+#endif /* CALVININPROGRESS_RFIDACTOR_H_ */
