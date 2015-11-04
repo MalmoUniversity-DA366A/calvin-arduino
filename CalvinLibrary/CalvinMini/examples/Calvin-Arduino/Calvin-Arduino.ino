7948c26a7d9f080379e73fdfe542deb7ac0b8347
aelvstam <andreas.elvstam@hotmail.com>
2015-11-04 21:24:43 +0100

    updated library, added example sketch

---
+++ CalvinLibrary/CalvinMini/examples/Calvin-Arduino/Calvin-Arduino.ino
@@ -0,0 +1,32 @@
+/* Calvin-Arduino example sketch
+
+  This example sketch shows how to setup a Calvin runtime on an Arduino Due board.   
+  The runtime allows the Arduino to be added to an existing Calvin network.
+  Afterwards an compatible actor can be deplayed onto the Arduino from a Calvin base. 
+  If aditional actors are descired one can easily modify the library to support this.
+  For further information regarding Calvin base, actor development and Calvin-Arduino please visit:
+  https://github.com/MalmoUniversity-DA366A/calvin-arduino
+
+*/
+
+#include <ArduinoJson.h>
+#include <CalvinMini.h>
+#include <SPI.h>
+#include <Ethernet.h>
+#include <LiquidCrystal.h>
+CalvinMini mini;
+
+void setup() {
+  byte macAdr[] = { 0x99, 0xAA, 0xBB, 0xCC, 0xDD, 0xFF };  // MAC address for the Ethernet shield
+  IPAddress ip(192,168,0,20);                              // Desired IP address for manual IP configuration
+  String rtID = "Calvin-Arduino";                          // Name of Calvin runtime
+  uint16_t port = 5002;                                    // Server incomming port
+  
+  Serial.begin(115200);                                    // Setup serial communication
+  Serial.println("On"); 
+  mini(rtID, macAdr, ip, port);                            // Setup Calvin mini
+}
+
+void loop() {
+  mini.loop();    // Run the calvin mini loop indefinitely
+}
